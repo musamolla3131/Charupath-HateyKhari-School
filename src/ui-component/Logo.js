fd6233cb3ef832df4c81ab04e15740b5ed16510d
Musa Molla <musamolla3131@gmail.com>
2023-02-03 22:30:49 +0600

    Logo issu solved

--- src/ui-component/Logo.js
+++ src/ui-component/Logo.js
@@ -1,6 +1,6 @@
 // material-ui
 import { useTheme } from '@mui/material/styles';
-import logo from 'assets/images/logo-dark.svg';
+import logo from 'assets/images/logo.svg';
 
 /**
  * if you want to use image instead of <svg> uncomment following.
