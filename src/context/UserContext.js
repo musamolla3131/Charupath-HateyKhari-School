acf313b43b8888a64e8cd4d2a012c319f813ab54
Musa Molla <musamolla3131@gmail.com>
2023-02-04 22:34:26 +0600

    User context

---
+++ src/context/UserContext.js
@@ -0,0 +1,73 @@
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const UserContext = ({ children }) => {
+
+    const [ loading, setLoading ] = useState(false);
+
+    const [ user, setUser ] = useState({});
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const userNameAndPhoto = (profile) => {
+        return updateProfile(auth.currentUser, profile)
+    };
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleprovider = new GoogleAuthProvider();
+
+
+    const googlePopup = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleprovider);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false);
+
+        })
+        return () => unsubscribe();
+    }, [ user ])
+
+    const authInfo = {
+        user,
+        createUser,
+        setUser,
+        signIn,
+        logOut,
+        googlePopup,
+        userNameAndPhoto,
+        loading
+    }
+    return (
+
+        <AuthContext.Provider value={authInfo} >
+            {children}
+        </AuthContext.Provider >
+    );
+};
+
+export default UserContext;
+
+// Test commetn
+// Path: src\Routes\PrivateRoute\PrivateRoute.js
+// Compare this snippet from src\Routes\Routes\Routes.js:
+// import React from 'react';
